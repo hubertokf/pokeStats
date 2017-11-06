06c156b08f3d6cf893fb3224c8e4e5493ed01b66
Huberto Kaiser Filho <hubertokf@gmail.com>
2017-11-06 14:19:50 -0200

    fixed bug on db

--- server/routes/db.js
+++ server/routes/db.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-app.get('/download', function (req, res) {
+router.get('/download', function (req, res) {
     var file = __dirname + '/db_bkp/bkp.sql';
     res.download(file); // Set disposition and send it.
 });
