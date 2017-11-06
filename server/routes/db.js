d743dd7858d6d9328bff6e24e1f7a4c079accd6e
Huberto Kaiser Filho <hubertokf@gmail.com>
2017-11-06 14:21:56 -0200

    fixed bkp file path

--- server/routes/db.js
+++ server/routes/db.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/download', function (req, res) {
-    var file = __dirname + '/db_bkp/bkp.sql';
+    var file = '../db_bkp/bkp.sql';
     res.download(file); // Set disposition and send it.
 });
 
